65664e2cac11ab7d21a38850c8a9beff305d31d3
Yoann Coualan <yoann.coualan@gmail.com>
2023-11-21 12:16:14 +0100

    maj update categorie

--- une_categorie.php
+++ une_categorie.php
@@ -9,14 +9,14 @@ if (isset($_GET['id'])) {
 	$categorie = new Categorie();
 	$produit = $categorie->findOneById($_GET['id']);
 
-	$update = true;
-	include 'form_categorie.php';
-
 	if ($produit == false) {
 		echo '<p>Produit introuvable</p>';
 	} else {
 		echo '<h1>' . $produit['nom'] . '</h1>';
 		echo '<p>' . $produit['description'] . '</p>';
+
+		$update = true;
+		include 'form_categorie.php';
 	}
 } else {
 	echo '<p>Aucune catégorie reçue</p>';
